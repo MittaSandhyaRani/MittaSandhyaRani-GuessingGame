db4ae33bc78f1634e94cf5354728fd93466e8d0b
MittaSandhyaRani <sandhyaranimitta8@gmail.com>
2024-12-11 12:54:51 +0530

    done

--- src/main/java/GameService.java
+++ src/main/java/GameService.java
@@ -1,11 +1,3 @@
-/**
- * This service class is intended to contain the basic logic for playing the number guessing game.
- * Separating menu logic and game logic is good for two reasons: it makes both classes as simple
- * as they could be, and it also makes this class capable of being tested with automated tests, since testing
- * a class that receives user input is difficult. If the logic of the game were to become more complicated,
- * the advantages of separating code in this way will become immediately apparent. You will see this in the
- * coming mini-projects.
- */
 public class GameService {
     /**
      * This variable represents a secret number. This variable can be referenced anywhere within this GameService class.
@@ -21,12 +13,13 @@ public class GameService {
     }
 
     /**
-     * TODO: this method should return true if guessNumber is equal to the secretNumber, and false otherwise.
+     * This method should return true if guessNumber is equal to the secretNumber, and false otherwise.
      *
      * @param guessNumber the number that the user has input into their console.
      * @return true if guessNumber is equal to the secretNumber, false otherwise.
      */
     public boolean checkGuess(double guessNumber) {
-        return false;
+        // Direct comparison of guessNumber with secretNumber
+        return guessNumber == secretNumber;
     }
 }
